cc348aedb4203d6c60aceb658543073e1cde7459
Michael Feron <feron.michael@gmail.com>
2020-06-03 14:21:14 -0600

    Found duplicate experiments. Added some preliminary plots.

--- src/init.R
+++ src/init.R
@@ -23,6 +23,23 @@ data$experiment <- as.factor(data$experiment)
 rm(temp)
 
 
+## Experiments 2, 11 and 1, 6 appear to be identical.
+data %>%
+  filter(experiment == 2 | experiment == 11) %>%
+  select(-time, -permeate_tank_level, -feed_cleaning_valve_state,
+         -permeate_discharge_valve_state, -system_mode) %>%
+  group_by(experiment) %>%
+  summarise_all(mean) %>%
+  as.data.frame
+data %>%
+  filter(experiment == 1 | experiment == 6) %>%
+  select(-time, -permeate_tank_level, -feed_cleaning_valve_state,
+         -permeate_discharge_valve_state, -system_mode) %>%
+  group_by(experiment) %>%
+  summarise_all(mean) %>%
+  as.data.frame
+
+
 ## Checking for constant frequency for each experiment.
 for (i in 1:11) {
   print(i)
@@ -38,6 +55,19 @@ ggplot(data, aes(x = feed_pressure_psi, color = experiment)) +
   labs(subtitle = 'by experiment', color = 'Exp.') +
   xlab('Feed Pressure (psi)') + ylab('Density') +
   theme_minimal()
+
+
+## Some plots of conductivity.
+data %>%
+  filter(experiment == 2) %>%
+  ggplot(aes(x = time, y = permeate_conductivity_high_us)) +
+  geom_line(color = 'steelblue') +
+  geom_line(aes(x = time, y = permeate_conductivity_low_us), color = 'orangered3')
+
+data %>%
+  ggplot(aes(x = time, y = permeate_conductivity_high_us)) +
+  geom_line(color = 'steelblue') +
+  facet_grid(rows = vars(experiment))
 
 
 

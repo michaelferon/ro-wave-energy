e7ec102e15adacf6d0524dfae222414ae7962569
Jonathan Yun <sukwonyun@gmail.com>
2020-06-04 11:14:52 -0500

    Changes to .R file

--- src/DataSplitR.R
+++ src/DataSplitR.R
@@ -1,59 +1,43 @@
+
+#load library
 library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
 
-
-
-load("/Users/jonathanyun/Downloads/roenergyMoWater.rda")
+#load data
+load("/Users/jonathanyun/Desktop/ro-wave-energy/data/roenergyMoWater.rda")
 View(fulldata)
 
-
-exp1 <- which(fulldata$experiment == 1)
-exp2 <- which(fulldata$experiment == 2)
-exp3 <- which(fulldata$experiment == 3)
-exp4 <- which(fulldata$experiment == 4)
-exp5 <- which(fulldata$experiment == 5)
-exp6 <- which(fulldata$experiment == 6)
-exp7 <- which(fulldata$experiment == 7)
-exp8 <- which(fulldata$experiment == 8)
-exp9 <- which(fulldata$experiment == 9)
-exp10 <- which(fulldata$experiment == 10)
-exp11 <- which(fulldata$experiment == 11)
+data <- fulldata %>% as_tibble
 
 
-ExperimentData1 <- fulldata[exp1,]
-View(ExperimentData1)
+data$time <- paste(data$date, data$time) %>%
+  parse_date_time('%Y-%m-%d %H:%M:%S %p')
 
-ExperimentData2 <- fulldata[exp2,]
-View(ExperimentData2)
+data <- data %>%
+          select(-date)
+data$experiment <- as.factor(data$experiment)
+View(data)
 
-ExperimentData3 <- fulldata[exp3,]
-View(ExperimentData3)
+#get rid of duplicate data rows
+which(data$experiment == 6)
+which(data$experiment == 11)
 
-ExperimentData4 <- fulldata[exp4,]
-View(ExperimentData4)
+data <- data %>%
+  filter(experiment != 6 & experiment != 11) %>%
+  select(-iteration, -permeate_tank_level, -feed_cleaning_valve_state, -permeate_discharge_valve_state)
 
-ExperimentData5 <- fulldata[exp5,]
-View(ExperimentData5)
+unique(data$experiment)
 
-ExperimentData6 <- fulldata[exp6,]
-View(ExperimentData6)
+data %>%
+  filter(experiment == 2) %>%
+  ggplot(aes(x = time, y = permeate_conductivity_high_us)) +
+  geom_line(color = 'steelblue') +
+  geom_line(aes(x = time, y = permeate_conductivity_low_us), color = 'orangered3')
 
-ExperimentData7 <- fulldata[exp7,]
-View(ExperimentData7)
 
-ExperimentData8 <- fulldata[exp8,]
-View(ExperimentData8)
-
-ExperimentData9 <- fulldata[exp9,]
-View(ExperimentData9)
-
-ExperimentData10 <- fulldata[exp10,]
-View(ExperimentData10)
-
-ExperimentData11 <- fulldata[exp11,]
-View(ExperimentData11)
+View(data)
 
 
 

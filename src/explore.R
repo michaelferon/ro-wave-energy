0a3c858f04cb3e12a3bd54ed411fabddd206574f
Michael Feron <feron.michael@gmail.com>
2020-06-09 13:37:06 -0600

    Added water permeability coefficient calculation to init.R.

--- src/explore.R
+++ src/explore.R
@@ -18,32 +18,23 @@ OUTPUT <- FALSE
 ## Experiments summary.
 tapply(data$experiment, data$experiment, length)
 (data.mean <- data %>%
-  select(-time, -runtime_h, -system_mode) %>%
+  select(-time, -system_mode) %>%
   group_by(experiment) %>%
   summarise_all(mean, na.rm = TRUE))
 (data.sd <- data %>%
-    select(-time, -runtime_h, -system_mode) %>%
+    select(-time, -system_mode) %>%
     group_by(experiment) %>%
     summarise_all(sd, na.rm = TRUE))
 (data.iqr <- data %>%
-    select(-time, -runtime_h, -system_mode) %>%
+    select(-time, -system_mode) %>%
     group_by(experiment) %>%
     summarise_all(IQR, na.rm = TRUE))
 (data.mad <- data %>%
-    select(-time, -runtime_h, -system_mode) %>%
+    select(-time, -system_mode) %>%
     group_by(experiment) %>%
     summarise_all(mad, na.rm = TRUE))
 
 
-
-
-## Feed Pressure distribution by experiment.
-ggplot(data, aes(x = feed_pressure_psi, color = experiment)) +
-  geom_density() +
-  ggtitle('Feed Pressure Distribution') +
-  labs(subtitle = 'by experiment', color = 'Exp.') +
-  xlab('Feed Pressure (psi)') + ylab('Density') +
-  theme_minimal()
 
 
 ## Scatterplot matrices.
@@ -78,9 +69,8 @@ for (i in 1:N) {
 
 
 ## Boxplot by experiment.
-del <- c(1, 2, 3, 8, 13, 14, 16, 17, 18, 19)
-del <- c('time', 'runtime_h', 'permeate_conductivity_high_us',
-         'reject_valve_open_pct', 'system_mode', 'experiment', 'ac_current_a')
+del <- c('time', 'permeate_conductivity_high_us', 'reject_valve_open_pct',
+         'system_mode', 'experiment', 'ac_current_a')
 vars <- names(data)[!(names(data) %in% del)]
 for (var in vars) {
   boxplot(data[[var]] ~ data$experiment, xlab = 'Experiment', ylab = var,
@@ -90,7 +80,7 @@ for (var in vars) {
 
 ## Simple linear model.
 model <- data %>%
-  select(-time, -runtime_h, -permeate_conductivity_high_us, -reject_conductivity_ms,
+  select(-time, -permeate_conductivity_high_us, -reject_conductivity_ms,
          -experiment, -ac_current_a) %>%
   lm(permeate_conductivity_low_us ~ ., data = .)
 summary(model)
--- src/init.R
+++ src/init.R
@@ -23,9 +23,8 @@ data$time <- paste(data$date, data$time) %>%
 # and 'permeate_discharge_valve_state' have only one factor value.
 del <- c('permeate_tank_level', 'feed_cleaning_valve_state',
          'permeate_discharge_valve_state', 'temperature_valve_open_pct')
-for (i in del) { print(length(unique(data[[i]]))) }
 data <- data %>%
-  select(-date, -iteration, -all_of(del))
+  select(-date, -iteration, -runtime_h, -total_permeate_l, -all_of(del))
 rm(del)
 
 
@@ -52,6 +51,21 @@ for (i in 7:10) {
 # Factor variables.
 data <- data %>%
   mutate_at(c('system_mode', 'experiment'), as.factor)
+
+
+
+
+## Calculating the water permiability coefficient.
+water_perm_coeff <- function(flux, feed_press, reject_cond, temp){
+  pi_r <- ((1000 * reject_cond)/(2 * 35500)) * 0.08313716 * (temp + 273.15)
+  coeff <- flux / (feed_press - (27.57094 + pi_r)/2)
+}
+data <- data %>%
+  mutate(water_perm = water_perm_coeff( water_flux_lmh,
+                                        feed_pressure_psi,
+                                        reject_conductivity_ms,
+                                        feed_temperature_c
+                                      ))
 
 
 ## Save new data to data.Rdata.
